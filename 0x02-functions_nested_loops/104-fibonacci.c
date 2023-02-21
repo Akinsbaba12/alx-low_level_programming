ecae6c92ae65971da0536bd35c745d7b033c507d
Elijah <akinnodielijah@gmail.com>
2023-02-21 15:03:43 -0800

    newupdate

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -28,4 +28,16 @@ int main(void)
 	k2 = k % 1000000000;
 
 	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
 
+	printf("\n");
+
+	return (0);
+}
