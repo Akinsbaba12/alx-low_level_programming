de041ea5216db2401e7b1a595af6d921d754b35a
Elijah <akinnodielijah@gmail.com>
2023-04-27 18:20:10 -0700

    latest update on task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,47 @@
 #include "main.h"
 
 /**
- * print_binary - Convert dec to binary
- * @n: Number
+ * _craze - calculate (eli and craze)
+ * @eli: eli of the exponet
+ * @crz: craze of the exponet
+ * Return: value of eli and craze
  */
+unsigned long int _craze(unsigned int eli, unsigned int crz)
+{
+	unsigned long int num;
+	unsigned int i;
 
+	num = 1;
+	for (i = 1; i <= crz; i++)
+		num *= eli;
+	return (num);
+}
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: num of prented
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	int e, count = 0;
+	unsigned long int dev, result;
+	char flag;
 
-	for (e = 10; e >= 0; e--)
+	flag = 0;
+	dev = _craze(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (dev != 0)
 	{
-		if (n & (1 << e))
+		result = n & dev;
+		if (result == dev)
 		{
+			flag = 1;
 			_putchar('1');
-			count++;
+
 		}
-		else if (count)
+		else if (flag == 1 || dev == 1)
+		{
 			_putchar('0');
+		}
+		dev >>= 1;
 	}
-	if (!count)
-		_putchar('0');
 }
